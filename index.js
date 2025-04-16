7edf398f6f51ac3ed590bb42df48e5f9aa18210c
Zarcotech <zidan.harb2012@gmail.com>
2025-04-16 16:18:09 -0500

    Pushing all code

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+const port = 3131;
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname + '/public' + '/templates' + '/index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
